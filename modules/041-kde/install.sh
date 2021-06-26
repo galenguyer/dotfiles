19b626dbcf0ffa5432bf73a0d15caefa62333d08
Galen Guyer <galen@galenguyer.com>
2021-06-26 00:05:16 -0700

    update kde config stuff

--- modules/041-kde/install.sh
+++ modules/041-kde/install.sh
@@ -3,6 +3,15 @@ source $DOT_ROOT/constants.sh
 DOT_MODULE="kde"
 
 log info setting kde configs
+
+log silly linking .config/kdedefaults
+rm -r "$HOME/.config/kdedefaults"
+ln -s "$DOT_ROOT/modules/041-kde/.config/kdedefaults" "$HOME/.config/kdedefaults"
+
+log silly linking .config/kdeglobals
+rm -r "$HOME/.config/kdeglobals"
+ln -s "$DOT_ROOT/modules/041-kde/.config/kdeglobals" "$HOME/.config/kdeglobals"
+
+log silly linking .kde4
 rm -r "$HOME/.kde4"
-log silly linking kde
 ln -s "$DOT_ROOT/modules/041-kde/.kde4" "$HOME/.kde4"
