88f38a89197eaa440d6efde8eea7f48bcaa457ad
Galen Guyer <galen@galenguyer.com>
2021-06-25 23:52:42 -0700

    improve zshrc installation

--- modules/040-zsh/install.sh
+++ modules/040-zsh/install.sh
@@ -2,4 +2,13 @@
 source $DOT_ROOT/constants.sh
 DOT_MODULE="zsh"
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [[ ! -f "$HOME/.zshrc" ]]; then
+    log info installing oh-my-zsh
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+log info applying zshrc
+log silly backing up zshrc
+[ -e "$HOME/.zshrc" ] && [ ! -e "$HOME/.zshrc.bak" ] && cp "$HOME/.zshrc" "$HOME/.zshrc.bak"
+rm "$HOME/.zshrc"
+log silly linking zshrc
+ln -s "$DOT_ROOT/modules/040-zsh/.zshrc" "$HOME/.zshrc"
