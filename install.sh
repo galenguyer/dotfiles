9e7102d9284a56d242b8f93ae0eb4edb6dfacde5
Galen Guyer <galen@galenguyer.com>
2021-06-26 00:31:12 -0700

    add dots only option

--- install.sh
+++ install.sh
@@ -6,12 +6,25 @@ export DOT_ROOT=$(pwd)
 source $DOT_ROOT/constants.sh
 
 # keep sudo alive
-sudo -v
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+if [[ ! $EUID -eq 0 ]]; then
+    log info "starting sudo"
+    sudo -v
+    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+fi
 
-for module in $DOT_ROOT/modules/**"$*"/install.sh; do
-    . $module
-done
+if [ "$1" = "dots" ]; then
+    log info "installing only dots"
+    dots=("zsh" "git")
+    for dot in $dots; do
+        for module in $DOT_ROOT/modules/**"$dot"/install.sh; do
+            . $module
+        done
+    done
+else
+    for module in $DOT_ROOT/modules/**"$*"/install.sh; do
+        . $module
+    done
+fi
 
 # unset DOT_SPLASH for future runs
 unset DOT_SPASH
