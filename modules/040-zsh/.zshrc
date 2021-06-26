3df2d89487e4ac378ccdffd74c493b7d277de50d
Galen Guyer <galen@galenguyer.com>
2021-06-26 00:17:00 -0700

    add a few zsh plugins

--- modules/040-zsh/.zshrc
+++ modules/040-zsh/.zshrc
@@ -70,7 +70,7 @@ export ZSH="/home/chef/.oh-my-zsh"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+plugins=(git zsh-autosuggestions zsh-autocompletions zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
 
--- modules/040-zsh/install.sh
+++ modules/040-zsh/install.sh
@@ -6,9 +6,42 @@ if [[ ! -f "$HOME/.zshrc" ]]; then
     log info installing oh-my-zsh
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
 log info applying zshrc
 log silly backing up zshrc
 [ -e "$HOME/.zshrc" ] && [ ! -e "$HOME/.zshrc.bak" ] && cp "$HOME/.zshrc" "$HOME/.zshrc.bak"
-rm "$HOME/.zshrc"
+
 log silly linking zshrc
+rm "$HOME/.zshrc"
 ln -s "$DOT_ROOT/modules/040-zsh/.zshrc" "$HOME/.zshrc"
+
+log info adding zsh plugins
+log silly adding zsh-users/zsh-completions
+if [[ ! -d "$HOME/.oh-my-zsh/plugins/custom/zsh-completions" ]]; then
+    git clone https://github.com/zsh-users/zsh-completions "$HOME/.oh-my-zsh/plugins/custom/zsh-completions"
+else
+    pwd=$(pwd)
+    cd "$HOME/.oh-my-zsh/plugins/custom/zsh-completions"
+    git pull
+    cd $pwd
+fi
+
+log silly adding zsh-users/zsh-autosuggestions
+if [[ ! -d "$HOME/.oh-my-zsh/plugins/custom/zsh-autosuggestions" ]]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/plugins/custom/zsh-autosuggestions"
+else
+    pwd=$(pwd)
+    cd "$HOME/.oh-my-zsh/plugins/custom/zsh-autosuggestions"
+    git pull
+    cd $pwd
+fi
+
+log silly adding zsh-users/zsh-syntax-highlighting
+if [[ ! -d "$HOME/.oh-my-zsh/plugins/custom/zsh-syntax-highlighting" ]]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting "$HOME/.oh-my-zsh/plugins/custom/zsh-syntax-highlighting"
+else
+    pwd=$(pwd)
+    cd "$HOME/.oh-my-zsh/plugins/custom/zsh-syntax-highlighting"
+    git pull
+    cd $pwd
+fi
