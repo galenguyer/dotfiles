b14af41cce35a010de2cc6edd4fb7409f9b0ef0f
Galen Guyer <galen@galenguyer.com>
2021-06-26 08:28:56 -0700

    add konsole colorscheme

--- modules/041-kde/install.sh
+++ modules/041-kde/install.sh
@@ -4,14 +4,17 @@ DOT_MODULE="kde"
 
 log info setting kde configs
 
-log silly linking .config/kdedefaults
+log silly "linking konsole configurations"
+ln -s -f "$DOT_ROOT/modules/041-kde/konsole" "$HOME/.local/share/konsole/konsole"
+
+log silly "linking ~/.config/kdedefaults"
 rm -r "$HOME/.config/kdedefaults"
 ln -s "$DOT_ROOT/modules/041-kde/.config/kdedefaults" "$HOME/.config/kdedefaults"
 
-log silly linking .config/kdeglobals
+log silly "linking ~/.config/kdeglobals"
 rm -r "$HOME/.config/kdeglobals"
 ln -s "$DOT_ROOT/modules/041-kde/.config/kdeglobals" "$HOME/.config/kdeglobals"
 
-log silly linking .kde4
+log silly "linking ~/.kde4"
 rm -r "$HOME/.kde4"
 ln -s "$DOT_ROOT/modules/041-kde/.kde4" "$HOME/.kde4"
---
+++ modules/041-kde/konsole/Profile 1.profile
@@ -0,0 +1,7 @@
+[Appearance]
+ColorScheme=Dracula
+Font=Source Code Pro,11,-1,5,50,0,0,0,0,0
+
+[General]
+Name=Profile 1
+Parent=FALLBACK/
