0335b4dd84cd53aceed3d513cf239f0294bccc3d
Galen Guyer <galen@galenguyer.com>
2021-06-27 04:40:16 +0000

    add debian nanorc and fix installer

--- modules/040-nano/install.sh
+++ modules/040-nano/install.sh
@@ -9,5 +9,12 @@ if [ "$DOT_OS" = "linux_arch" ]; then
     [ -e "$HOME/.nanorc" ] && [ ! -e "$HOME/.nanorc.bak" ] && cp "$HOME/.nanorc" "$HOME/.nanorc.bak"
     log silly linking nanorc
     rm "$HOME/.nanorc"
-    ln -s "$DOT_ROOT/modules/040-nano/.nanorc" "$HOME/.nanorc"
+    ln -s "$DOT_ROOT/modules/040-nano/.nanorc.arch" "$HOME/.nanorc"
+fi
+if [ "$DOT_OS" = "linux_debian" ]; then
+    log silly backing up nanorc
+    [ -e "$HOME/.nanorc" ] && [ ! -e "$HOME/.nanorc.bak" ] && cp "$HOME/.nanorc" "$HOME/.nanorc.bak"
+    log silly linking nanorc
+    rm "$HOME/.nanorc"
+    ln -s "$DOT_ROOT/modules/040-nano/.nanorc.debian" "$HOME/.nanorc"
 fi
