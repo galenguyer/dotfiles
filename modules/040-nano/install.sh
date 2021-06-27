4e157a68f06071de639f4a37d004287266c3bda5
Galen Guyer <galen@galenguyer.com>
2021-06-26 18:52:44 -0700

    specify arch specific nanorc

--- modules/040-nano/install.sh
+++ modules/040-nano/install.sh
@@ -3,9 +3,11 @@ source $DOT_ROOT/constants.sh
 DOT_MODULE="nano"
 
 log info applying nanorc
-log silly backing up nanorc
-[ -e "$HOME/.nanorc" ] && [ ! -e "$HOME/.nanorc.bak" ] && cp "$HOME/.nanorc" "$HOME/.nanorc.bak"
 
-log silly linking nanorc
-rm "$HOME/.nanorc"
-ln -s "$DOT_ROOT/modules/040-nano/.nanorc" "$HOME/.nanorc"
+if [ "$DOT_OS" = "linux_arch" ]; then
+    log silly backing up nanorc
+    [ -e "$HOME/.nanorc" ] && [ ! -e "$HOME/.nanorc.bak" ] && cp "$HOME/.nanorc" "$HOME/.nanorc.bak"
+    log silly linking nanorc
+    rm "$HOME/.nanorc"
+    ln -s "$DOT_ROOT/modules/040-nano/.nanorc" "$HOME/.nanorc"
+fi
